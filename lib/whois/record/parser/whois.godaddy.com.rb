65f6ad32434710baa3a7952b320d301e5397b78a
David Hillard <dhillard@socialogue.com>
2013-01-21 14:50:59 -0800

    fixing bugs in GoDaddy parser

--- lib/whois/record/parser/whois.godaddy.com.rb
+++ lib/whois/record/parser/whois.godaddy.com.rb
@@ -63,7 +63,7 @@ module Whois
         end
 
         property_supported :registrant_contacts do
-          build_contact('Registrant:', Record::Contact::TYPE_REGISTRANT)
+          build_registrant_contact('Registrant:', Record::Contact::TYPE_REGISTRANT)
         end
 
         property_supported :admin_contacts do
@@ -86,6 +86,41 @@ module Whois
 
       private
 
+      def build_registrant_contact(element, type)
+        match = content_for_scanner.slice(/#{element}\n((.+\n)+)\n/, 1)
+        return unless match
+
+        # Lines 1 and 5 may be absent, depending on the record.
+        # The parser attempts to correct for this, but may be a bit flaky
+        # on non-standard data.
+        #
+        # 0 GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
+        # 1 GoDaddy.com, Inc.
+        # 2 14455 N Hayden Rd Suite 219
+        # 3 Scottsdale, Arizona 85260
+        # 4 United States
+        # 5 +1.4805058800      Fax -- +1.4805058844
+
+        lines = $1.split("\n").map(&:strip)
+        full_address = lines.length >= 4 ? lines[-3] : ""
+        full_address =  lines[-4] + "\n" +full_address if lines.length >= 5
+
+        Record::Contact.new(
+          :type         => type,
+          :id           => nil,
+          :name         => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
+          :organization => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
+          :address      => full_address,
+          :city         => lines.length >= 4 ? lines[-2].to_s.partition(",")[0] : "",
+          :zip          => lines.length >= 4 ? lines[-2].to_s.rpartition(" ")[2] : "",
+          :state        => lines.length >= 4 ? lines[-2].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
+          :country      => lines.length >= 4 ? lines[-1] : "",
+          :phone        => nil,
+          :fax          => nil,
+          :email        => lines[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first
+        )
+      end
+
         def build_contact(element, type)
           match = content_for_scanner.slice(/#{element}\n((.+\n)+)\n/, 1)
           return unless match
@@ -102,25 +137,29 @@ module Whois
           # 5 +1.4805058800      Fax -- +1.4805058844
 
           lines = $1.split("\n").map(&:strip)
+          full_address = lines.length >= 5 ? lines[-4].to_s : ""
+          full_address =  lines[-5].to_s + "\n" +full_address if lines.length >= 6
 
           phone = nil
           fax   = nil
           if lines[-1].to_s =~ /Fax --/
-             phone, fax = lines.delete_at(-1).to_s.scan(/^(.*) Fax --(.*)$/).first
-             phone = phone.strip
-             fax   = fax.strip
+            phone, fax = lines.delete_at(-1).to_s.scan(/^(.*) Fax --(.*)$/).first
+            phone = phone.strip
+            fax   = fax.strip
+          else
+            phone = lines[-1].to_s.strip
           end
 
           Record::Contact.new(
             :type         => type,
             :id           => nil,
             :name         => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-            :organization => lines.length >= 5 ? lines[-4] : "",
-            :address      => lines.length >= 4 ? lines[-3] : "",
-            :city         => lines.length >= 4 ? lines[-2].to_s.partition(",")[0] : "",
-            :zip          => lines.length >= 4 ? lines[-2].to_s.rpartition(" ")[2] : "",
-            :state        => lines.length >= 4 ? lines[-2].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
-            :country      => lines.length >= 4 ? lines[-1] : "",
+            :organization => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
+            :address      => full_address,
+            :city         => lines.length >= 4 ? lines[-3].to_s.partition(",")[0] : "",
+            :zip          => lines.length >= 4 ? lines[-3].to_s.rpartition(" ")[2] : "",
+            :state        => lines.length >= 4 ? lines[-3].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
+            :country      => lines.length >= 4 ? lines[-2] : "",
             :phone        => phone,
             :fax          => fax,
             :email        => lines[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first
