dcb01fee5d627f2445104f9f43a56d474f38f191
David Hillard <dhillard@socialogue.com>
2013-08-07 18:36:43 -0700

    Updating GoDaddy parser

--- lib/whois/record/parser/whois.godaddy.com.rb
+++ lib/whois/record/parser/whois.godaddy.com.rb
@@ -54,75 +54,41 @@ module Whois
         #   end
         # end
 
-
+        #Rails.logger.debug("GODADDY - called")
         property_supported :registrar do
           Record::Registrar.new(
-            :name => content_for_scanner[/Registered through: (.+)\n/, 1],
+            :name => content_for_scanner[/Registrar Expiration Date:(.+)\n/, 1],
             :url => "http://www.godaddy.com/"
           )
         end
 
         property_supported :registrant_contacts do
-          build_registrant_contact('Registrant:', Record::Contact::TYPE_REGISTRANT)
+          build_registrant_contact('Registrant Name:', Record::Contact::TYPE_REGISTRANT)
         end
 
         property_supported :admin_contacts do
-          build_contact('Administrative Contact:', Record::Contact::TYPE_ADMIN)
+          build_admin_contact('Admin Name:', Record::Contact::TYPE_ADMIN)
         end
 
         property_supported :technical_contacts do
-          build_contact('Technical Contact:', Record::Contact::TYPE_TECHNICAL)
+          build_tech_contact('Tech Name:', Record::Contact::TYPE_TECHNICAL)
         end
 
-
         property_supported :nameservers do
-          if content_for_scanner =~ /Domain servers in listed order:\n((.+\n)+)\n/
+          if content_for_scanner =~ /Name Server:\n((.+\n)+)\n/
             $1.split("\n").map do |line|
               Record::Nameserver.new(:name => line.strip)
+              #Rails.logger.debug("GODADDY - nameserver found = #{line.strip}")
             end
           end
         end
 
 
-      private
+        private
 
-      def build_registrant_contact(element, type)
-        match = content_for_scanner.slice(/#{element}\n((.+\n)+)\n/, 1)
-        return unless match
-
-        # Lines 1 and 5 may be absent, depending on the record.
-        # The parser attempts to correct for this, but may be a bit flaky
-        # on non-standard data.
-        #
-        # 0 GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
-        # 1 GoDaddy.com, Inc.
-        # 2 14455 N Hayden Rd Suite 219
-        # 3 Scottsdale, Arizona 85260
-        # 4 United States
-        # 5 +1.4805058800      Fax -- +1.4805058844
-
-        lines = $1.split("\n").map(&:strip)
-        full_address = lines.length >= 4 ? lines[-3] : ""
-        full_address =  lines[-4] + "\n" +full_address if lines.length >= 5
-
-        Record::Contact.new(
-          :type         => type,
-          :id           => nil,
-          :name         => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-          :organization => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-          :address      => full_address,
-          :city         => lines.length >= 4 ? lines[-2].to_s.partition(",")[0] : "",
-          :zip          => lines.length >= 4 ? lines[-2].to_s.rpartition(" ")[2] : "",
-          :state        => lines.length >= 4 ? lines[-2].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
-          :country      => lines.length >= 4 ? lines[-1] : "",
-          :phone        => nil,
-          :fax          => nil,
-          :email        => lines[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first
-        )
-      end
-
-        def build_contact(element, type)
-          match = content_for_scanner.slice(/#{element}\n((.+\n)+)\n/, 1)
+        def build_registrant_contact(element, type)
+          match = content_for_scanner.slice(/#{element}((.+\n)+)\n/, 1)
+          #Rails.logger.debug("GODADDY build_registrant_contact match = #{match}, element=#{element}, content=#{content_for_scanner}.")
           return unless match
 
           # Lines 1 and 5 may be absent, depending on the record.
@@ -137,37 +103,92 @@ module Whois
           # 5 +1.4805058800      Fax -- +1.4805058844
 
           lines = $1.split("\n").map(&:strip)
-          full_address = lines.length >= 5 ? lines[-4].to_s : ""
-          full_address =  lines[-5].to_s + "\n" +full_address if lines.length >= 6
-
-          phone = nil
-          fax   = nil
-          if lines[-1].to_s =~ /Fax --/
-            phone, fax = lines.delete_at(-1).to_s.scan(/^(.*) Fax --(.*)$/).first
-            phone = phone.strip
-            fax   = fax.strip
-          else
-            phone = lines[-1].to_s.strip
-          end
-
+        
           Record::Contact.new(
             :type         => type,
             :id           => nil,
-            :name         => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-            :organization => lines[0].to_s.gsub(/\s\S+@[^\.].*\.[a-z]{2,}\s?\)?$/, "").strip,
-            :address      => full_address,
-            :city         => lines.length >= 4 ? lines[-3].to_s.partition(",")[0] : "",
-            :zip          => lines.length >= 4 ? lines[-3].to_s.rpartition(" ")[2] : "",
-            :state        => lines.length >= 4 ? lines[-3].to_s.partition(",")[2].rpartition(" ")[0].to_s.strip : "",
-            :country      => lines.length >= 4 ? lines[-2] : "",
-            :phone        => phone,
-            :fax          => fax,
-            :email        => lines[0].to_s.scan(/[^\s]\S+@[^\.].*\.[a-z]{2,}[^\s\)\n]/).first
+            :name         => lines[0].rpartition("Registrant Name:")[2].strip, 
+            :organization => lines[1].rpartition("Registrant Organization:")[2].strip,
+            :address      => lines[2].rpartition("Registrant Street:")[2].strip,
+            :city         => lines[3].rpartition("Registrant City:")[2].strip,
+            :state        => lines[4].rpartition("Registrant State/Province:")[2].strip,
+            :zip          => lines[5].rpartition("Registrant Postal Code:")[2].strip,
+            :country      => lines[6].rpartition("Registrant Country:")[2].strip,
+            :email        => nil,
+            :phone        => nil,
+            :fax          => nil
+          )
+        end
+
+        def build_admin_contact(element, type)
+          match = content_for_scanner.slice(/#{element}((.+\n)+)\n/, 1)
+          #Rails.logger.debug("GODADDY build_admin_contact match = #{match}, element=#{element}, content=#{content_for_scanner}.")
+          return unless match
+  
+          # Lines 1 and 5 may be absent, depending on the record.
+          # The parser attempts to correct for this, but may be a bit flaky
+          # on non-standard data.
+          #
+          # 0 GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
+          # 1 GoDaddy.com, Inc.
+          # 2 14455 N Hayden Rd Suite 219
+          # 3 Scottsdale, Arizona 85260
+          # 4 United States
+          # 5 +1.4805058800      Fax -- +1.4805058844
+
+          lines = $1.split("\n").map(&:strip)
+        
+          Record::Contact.new(
+            :type         => type,
+            :id           => nil,
+            :name         => lines[0].rpartition("Admin Name:")[2].strip, 
+            :organization => lines[1].rpartition("Admin Organization:")[2].strip,
+            :address      => lines[2].rpartition("Admin Street:")[2].strip,
+            :city         => lines[3].rpartition("Admin City:")[2].strip,
+            :state        => lines[4].rpartition("Admin State/Province:")[2].strip,
+            :zip          => lines[5].rpartition("Admin Postal Code:")[2].strip,
+            :country      => lines[6].rpartition("Admin Country:")[2].strip,
+            :phone        => lines[7].rpartition("Admin Phone:")[2].strip,
+            :fax          => lines[8].rpartition("Admin Fax:")[2].strip,
+            :email        => lines[9].rpartition("Admin Email:")[2].strip
+          )
+        end
+
+        def build_tech_contact(element, type)
+          match = content_for_scanner.slice(/#{element}((.+\n)+)\n/, 1)
+          #Rails.logger.debug("GODADDY build_tech_contact match = #{match}, element=#{element}, content=#{content_for_scanner}.")
+          return unless match
+
+          # Lines 1 and 5 may be absent, depending on the record.
+          # The parser attempts to correct for this, but may be a bit flaky
+          # on non-standard data.
+          #
+          # 0 GoDaddy.com, Inc., GoDaddy.com, Inc.  dns@jomax.net
+          # 1 GoDaddy.com, Inc.
+          # 2 14455 N Hayden Rd Suite 219
+          # 3 Scottsdale, Arizona 85260
+          # 4 United States
+          # 5 +1.4805058800      Fax -- +1.4805058844
+
+          lines = $1.split("\n").map(&:strip)
+        
+          Record::Contact.new(
+            :type         => type,
+            :id           => nil,
+            :name         => lines[0].rpartition("Tech Name:")[2].strip, 
+            :organization => lines[1].rpartition("Tech Organization:")[2].strip,
+            :address      => lines[2].rpartition("Tech Street:")[2].strip,
+            :city         => lines[3].rpartition("Tech City:")[2].strip,
+            :state        => lines[4].rpartition("Tech State/Province:")[2].strip,
+            :zip          => lines[5].rpartition("Tech Postal Code:")[2].strip,
+            :country      => lines[6].rpartition("Tech Country:")[2].strip,
+            :phone        => lines[7].rpartition("Tech Phone:")[2].strip,
+            :fax          => lines[8].rpartition("Tech Fax:")[2].strip,
+            :email        => lines[9].rpartition("Tech Email:")[2].strip
           )
         end
 
       end
-
     end
   end
 end
